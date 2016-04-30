f8b003c88b51014502310683ec79db3fd6057bb9
Costanza Volpini <costanza.volpini@gmail.com>
2016-04-30 10:58:35 +0200

    Removed Floating Action Button from ByRoom
    TO DO: Implement the navigation bar also in the class ByRoom

--- AndroidStudio/USIRooms/app/src/main/java/com/usirooms/usirooms/byRooms.java
+++ AndroidStudio/USIRooms/app/src/main/java/com/usirooms/usirooms/byRooms.java
@@ -3,6 +3,8 @@ package com.usirooms.usirooms;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -16,14 +18,15 @@ public class byRooms extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+      /*  FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        }); */
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
