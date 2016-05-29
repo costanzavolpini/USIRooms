12f3d93ae303cca4f1d0b3fb4945c3ee06bca255
Costanza Volpini <costanza.volpini@gmail.com>
2016-05-30 00:44:31 +0200

    New themes

---
+++ AndroidStudio/USIROOM/app/src/main/java/com/usirooms/usirooms/dummy/roomTime.java
@@ -0,0 +1,16 @@
+package com.usirooms.usirooms.dummy;
+
+/**
+ * Created by costanzavolpini on 30/05/16.
+ */
+public class roomTime {
+    private String freeTime;
+
+    public roomTime(String freeTime){
+        this.freeTime = freeTime;
+    }
+
+   // public String getTime(){
+     //   return time;
+   // }
+}
