433b452c920158df371dcd0bb9cdef89ec9c49b6
Michele Lustro <lustrm@usi.ch>
2016-05-30 15:03:57 +0200

    EventCard added

--- AndroidStudio/USIROOM/app/src/main/java/com/usirooms/usirooms/singleEvent.java
+++ AndroidStudio/USIROOM/app/src/main/java/com/usirooms/usirooms/singleEvent.java
@@ -1,17 +1,35 @@
 package com.usirooms.usirooms;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import info.androidhive.recyclerview.roomAdapter;
 
 public class singleEvent extends AppCompatActivity {
+
+    TextView tv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_event);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        tv = (TextView) findViewById(R.id.single_event_room);
+
+        tv.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                tv.getContext().startActivity(new Intent(tv.getContext(), singleRoom.class));
+            }
+        });
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem menuItem)
