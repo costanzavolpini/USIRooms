a8ccfd84daf46305cf7b5e64127cc6492696df73
Costanza Volpini <costanza.volpini@gmail.com>
2016-05-31 06:22:29 +0200

    Added layout singleroom

--- AndroidStudio/USIROOM/app/src/main/java/com/usirooms/usirooms/singleRoom.java
+++ AndroidStudio/USIROOM/app/src/main/java/com/usirooms/usirooms/singleRoom.java
@@ -1,17 +1,26 @@
 package com.usirooms.usirooms;
 
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class singleRoom extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.SingleRoomTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_room);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        final Drawable upArrow = getResources().getDrawable(R.drawable.ic_cross);
+        upArrow.setColorFilter(getResources().getColor(R.color.gray), PorterDuff.Mode.SRC_ATOP);
+        getSupportActionBar().setHomeAsUpIndicator(upArrow);
     }
 
     @Override
